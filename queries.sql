02807aee347fbfadd8b9a3fe260b39495c1bd1b1
obibaadoma <akrasikonadu@yahoo.com>
2023-09-19 21:10:37 +0000

    Fixed issues raised

--- queries.sql
+++ queries.sql
@@ -21,6 +21,7 @@ SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL OR species = 'null';
+COMMIT;
 
 BEGIN;
 DELETE FROM animals
@@ -36,8 +37,13 @@ SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
 
-UPDATE animals
-SET weight_kg = weight_kg * -1;
+  BEGIN;
+    DELETE FROM animals WHERE  date_of_birth > '2022/01/01' ;
+    SAVEPOINT SP1;
+    UPDATE animals SET weight_kg = weight_kg * -1; 
+    ROLLBACK TO SP1;
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+    COMMIT;
 
 SELECT COUNT(*) AS animal_count
 FROM animals;
