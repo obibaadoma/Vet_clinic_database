13601bb1cf90f45ccb7781850982165455f7b6a4
obibaadoma <akrasikonadu@yahoo.com>
2023-09-20 10:33:50 +0000

    changed data.sql

--- data.sql
+++ data.sql
@@ -10,7 +10,7 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUE
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Pikachu','2021-01-07',1,false,15.04);
 
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Charmander','2020-02-08',0,false,11),
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Charmander','2020-02-08',0,false,-11),
 ('Plantmon','2021-11-15',2,true,-5.7),
 ('Squirtle','1993-04-02',3,false,-12.13),
 ('Angemon','2005-06-12',1,true,-45),
